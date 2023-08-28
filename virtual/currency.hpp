ce287fba13d597f64e55e0e80bd9c5d869761b2a
Soldy <4786022+Soldy@users.noreply.github.com>
2023-08-28 23:31:30 +0100

    currency
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- virtual/currency.hpp
+++ virtual/currency.hpp
@@ -1,7 +1,17 @@
 #include <string>
 
-namespace fiscale{
+namespace pegaitivarn_currency{
 struct currency{
     std::string code;
+    int rate;
+};
+
+std::map<string, currency> currencies 
+double exchange (
+    int amount,
+    std::string base_code,
+    std::string target_code
+){
+    return (amount/(currencies[base_code].rate)*currencies[target_code].rate);
 };
 }
